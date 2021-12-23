a4e6a7005169d5552de2ab5932e794684b72f413
Radu <raduionut.pirlog@gmail.com>
2021-12-24 01:45:58 +0200

    Implemented first menu page

---
+++ app/src/main/java/com/example/carcollector/EuropeMenu.kt
@@ -0,0 +1,11 @@
+package com.example.carcollector
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EuropeMenu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_europe_menu)
+    }
+}
