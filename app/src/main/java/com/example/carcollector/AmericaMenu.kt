22c0db063d2b9b65ac515b71bea3f603a9178ca6
Radu <raduionut.pirlog@gmail.com>
2022-01-06 00:44:24 +0200

    Implemented America, Asia and Oceania countries page Updated car list Will rethink the layout

---
+++ app/src/main/java/com/example/carcollector/AmericaMenu.kt
@@ -0,0 +1,11 @@
+package com.example.carcollector
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AmericaMenu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_america_menu)
+    }
+}
---
+++ app/src/main/java/com/example/carcollector/AsiaMenu.kt
@@ -0,0 +1,11 @@
+package com.example.carcollector
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AsiaMenu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_asia_menu3)
+    }
+}
